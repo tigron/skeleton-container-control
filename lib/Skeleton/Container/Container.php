50487da3c2ceb5c90befdbfa87939b38e1f75a62
Christophe Gosiau <christophe@tigron.be>
2019-01-16 13:24:10 +0100

    first commit: create composer package

---
+++ lib/Skeleton/Container/Container.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Container class
+ *
+ * @author Christophe Gosiau <christophe@tigron.be>
+ * @author Gerry Demaret <gerry@tigron.be>
+ */
+
+namespace Skeleton\Container;
+
+class Container {
+
+}
